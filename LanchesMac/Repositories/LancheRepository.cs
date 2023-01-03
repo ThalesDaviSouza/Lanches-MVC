fc30e8f56538a995eb6c61175e2bf6826f4e8892
ThalesDaviSouza <thalesdavisoul@gmail.com>
2023-01-03 18:19:46 -0300

    Adicionado um repository para lanches

---
+++ LanchesMac/Repositories/LancheRepository.cs
@@ -0,0 +1,30 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LancheRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(lanche => lanche.Categoria);
+
+        public IEnumerable<Lanche> LanchesFavoritos => (from lanche in _context.Lanches
+                                                       where lanche.IsLanchePreferido
+                                                       select lanche).Include(l => l.Categoria);
+
+        public Lanche GetLancheById(int lancheId)
+        {
+            Lanche lanche = _context.Lanches.FirstOrDefault(lanche => lanche.LancheId == lancheId);
+
+            return lanche;
+        }
+    }
+}
