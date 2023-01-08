7b6334dadcdec68df4bb526fdb0170211b960b1d
ThalesDaviSouza <thalesdavisoul@gmail.com>
2023-01-08 16:19:31 -0300

    Adicionado CarrinhoCompraController

---
+++ LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -0,0 +1,57 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class CarrinhoCompraController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraController(ILancheRepository lancheRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _lancheRepository = lancheRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IActionResult Index()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens().Result;
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            var carrinhoCompraViewModel = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal().Result
+            };
+
+            return View(carrinhoCompraViewModel);
+        }
+
+        public RedirectToActionResult AdicionarItemNoCarrinho(int lancheId)
+        {
+            var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+
+            if(lancheSelecionado != null)
+            {
+                _carrinhoCompra.AdicionarAoCarrinho(lancheSelecionado);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult RemoverItemDoCarrinhoCompra(int lancheId)
+        {
+            var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+
+            if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.RemoverDoCarrinho(lancheSelecionado);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
