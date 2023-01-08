ffd7219b7b821a54298c31d6b0fd1bd9e4e7bcd2
ThalesDaviSouza <thalesdavisoul@gmail.com>
2023-01-08 17:59:54 -0300

    Adicionando exibição dos favoritos na tela de inicio

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,9 +8,21 @@ namespace LanchesMac.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILancheRepository _lancheRepository;
+
+        public HomeController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var viewModel = new HomeViewModel
+            {
+                LanchesPreferidos = _lancheRepository.LanchesFavoritos
+            };
+
+            return View(viewModel);
         }
         public IActionResult Privacy()
         {
