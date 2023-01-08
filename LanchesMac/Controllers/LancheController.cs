ddce29934eff36176d2d82e8800db2b7d40d2f86
ThalesDaviSouza <thalesdavisoul@gmail.com>
2023-01-08 10:54:10 -0300

    Adicionando um ViewModel para listar os lanches

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -13,8 +14,13 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
-            var lanches = _lancheRepository.Lanches;
-            return View(lanches);
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+
+            LancheListViewModel viewModel = new LancheListViewModel();
+            viewModel.Lanches = _lancheRepository.Lanches;
+            viewModel.CategoriaAtual = "Categoria Atual";
+            return View(viewModel);
         }
     }
 }
